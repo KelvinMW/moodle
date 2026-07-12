c624a777bd4bbbd6418d49caf49ab532fd81069c
KelvinMW <wkelvinmaina@gmail.com>
2026-07-12 22:00:52 +0400

    Login hero: light text for legibility on dark background

--- public/theme/oneboard/version.php
+++ public/theme/oneboard/version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026071203;
+$plugin->version   = 2026071204;
 $plugin->requires  = 2026041000;
 $plugin->component = 'theme_oneboard';
 $plugin->dependencies = [
